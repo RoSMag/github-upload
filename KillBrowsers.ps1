7a586953105ca4228e9a384046d3c9493cc6d174
RoSMag <47318722+RoSMag@users.noreply.github.com>
2019-02-11 10:01:07 +0300

    Create KillBrowsers.ps1
    Initializing repository

---
+++ KillBrowsers.ps1
@@ -0,0 +1,5 @@
+$cab = 'k417' ; $comps = 'k417comp01','k417comp02','k417comp03','k417comp04','k417comp05','k417comp06','k417comp07','k417comp08','k417comp09','k417comp10','k417comp11','k417comp12','k417comp13','k417comp14','k417comp15'
+Invoke-Command -ComputerName $comps -ScriptBlock { stop-process -Name firefox -Force }
+Invoke-Command -ComputerName $comps -ScriptBlock { stop-process -Name chrome -Force }
+Invoke-Command -ComputerName $comps -ScriptBlock { stop-process -Name MicrosoftEdgeCP -Force }
+Invoke-Command -ComputerName $comps -ScriptBlock { stop-process -Name iexplore -Force }
